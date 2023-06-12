1f94759a8c0411f8702e7d9e3b9ddc335636ad0e
Samriddhi Shreyasee <112820579+Samiriddhi@users.noreply.github.com>
2023-06-12 20:14:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 function hasKey(key) {
   //   write your code here
        for(let k in sampleObject ){
-		   if(sampleObject.k==key){
+		   if(k==key){
 			   return true;
 		   }
 	   
